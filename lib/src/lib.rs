a3d0f471ca34e459b7a435490483e6a9fda3fbc1
Juyeong Maing <mjy9088@naver.com>
2024-07-07 00:22:10 +0900

    remove debug messages

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,13 @@
 use std::{
     cmp::min_by_key,
     collections::{BinaryHeap, HashMap},
-    fmt::Debug,
 };
 
 use graph::{GraphDistance, GraphEdge, GraphNode};
 
 pub mod graph;
 
-pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
+pub fn dijkstra<T, D: GraphDistance>(
     from: GraphNode<T, D>,
     to: GraphNode<T, D>,
 ) -> Option<Vec<GraphEdge<T, D>>> {
@@ -31,11 +30,8 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
     for edge in from.adjacent().nodes {
         to_visit.push(edge.to)
     }
-    println!("visited: {:?}", from);
-    println!("to_visit: {:?}", to_visit);
 
     while let Some(node_to_visit) = to_visit.pop() {
-        println!("visiting: {:?}", node_to_visit);
         // get new distance
         let (new_distance, new_move) = node_to_visit
             .reverse_adjacent()
@@ -47,7 +43,6 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
                     last_move,
                 }) = visited.get(&current.from)
                 {
-                    println!("one of edge to node to visit is already visited");
                     Some((
                         total_distance.clone() + current.distance.clone(),
                         GraphEdge {
@@ -61,18 +56,15 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
                         },
                     ))
                 } else {
-                    println!("one of edge to node to visit is not already visited");
                     None
                 };
 
-                let result = match (min, current) {
+                match (min, current) {
                     (Some(min), Some(current)) => Some(min_by_key(min, current, |d| d.0.clone())),
                     (Some(min), None) => Some(min),
                     (None, Some(current)) => Some(current),
                     (None, None) => None,
-                };
-                println!("  so, returning {:?}", result);
-                result
+                }
             })
             .unwrap();
 
@@ -89,10 +81,6 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
                 for edge in node_to_visit.adjacent().nodes {
                     to_visit.push(edge.to)
                 }
-                println!("visited: {:?}", node_to_visit);
-                println!("to_visit: {:?}", to_visit);
-            } else {
-                println!("not needed to visit.");
             }
         } else {
             // unconditionally visit if not already visited
@@ -106,13 +94,10 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
             for edge in node_to_visit.adjacent().nodes {
                 to_visit.push(edge.to)
             }
-            println!("visited: {:?}", node_to_visit);
-            println!("to_visit: {:?}", to_visit);
         }
     }
     // visited all connected
-    println!("visited all connected nodes.");
-    return if let Some(MapNode { last_move, .. }) = visited.get(&to) {
+    if let Some(MapNode { last_move, .. }) = visited.get(&to) {
         // from and to is connected
         Some(if let Some(last_move) = last_move {
             let mut last_move = Some(last_move.clone());
@@ -129,7 +114,7 @@ pub fn dijkstra<T: Debug, D: GraphDistance + Debug>(
     } else {
         // from and to is not connected
         None
-    };
+    }
 }
 
 #[cfg(test)]
